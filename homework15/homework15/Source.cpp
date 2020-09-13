b87c24e2dc5cc1c2d64e9047c2f2660c890a0b19
KuR0uSaGi <69112945+KuR0uSaGi@users.noreply.github.com>
2020-09-13 13:22:39 +0700

    Add files via upload

--- homework15/homework15/Source.cpp
+++ homework15/homework15/Source.cpp
@@ -1,35 +1,34 @@
 #include<stdio.h>
+int function(int,int*);
+
 int main() {
 	int data[] = { 10,20,30,40,50 };
 	int* ptr = &data[0];
-	int x, y;
+	int a, b;
 	printf("enter number :");
-	scanf_s("%d %d", &x, &y);
-	if (x > y && x > 0)
+	scanf_s("%d %d", &a, &b);
+	if (a > b && a > 0)
 	{
-		printf("%d \n", *(ptr) / x);
-		printf("%d \n", *(ptr + 1) / x);
-		printf("%d \n", *(ptr + 2) / x);
-		printf("%d \n", *(ptr + 3) / x);
-		printf("%d \n", *(ptr + 4) / x);
+		function(a,ptr);
 	}
-	if (x < 0 && y < 0)
+	if (a < 0 && b < 0)
 		printf("put number more than zero");
-	if(y>x && y>0)
+	if(b>a && b>0)
 	{
-		printf("%d \n", *(ptr) / y);
-		printf("%d \n", *(ptr + 1) / y);
-		printf("%d \n", *(ptr + 2) / y);
-		printf("%d \n", *(ptr + 3) / y);
-		printf("%d \n", *(ptr + 4) / x);
+		function(b,ptr);
 	}
-	if(y==x && x>0 &&y>0)
+	if(a==b && a>0 &&b>0)
 	{
-		printf("%d \n", *(ptr) / x);
-		printf("%d \n", *(ptr + 1) / x);
-		printf("%d \n", *(ptr + 2) / x);
-		printf("%d \n", *(ptr + 3) / x);
-		printf("%d \n", *(ptr + 4) / x);
+		function(a,ptr);
 	}
+	
+}
+int function(int x,int* y) 
+{
+	printf("%d \n", *(y) / x);
+	printf("%d \n", *(y + 1) / x);
+	printf("%d \n", *(y + 2) / x);
+	printf("%d \n", *(y + 3) / x);
+	printf("%d \n", *(y + 4) / x);
 	return 0;
 }
